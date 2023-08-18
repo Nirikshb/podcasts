ef74683ab1524b75771ee875e54bc1bcb4baa26e
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-08-18 21:07:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route element={<PrivateRoutes />} />
+          {/* Profile
+          Podcasts */}
         </Routes>
       </Router>
       
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+  return (
+    <div className="navbar">
+        <div className="links">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header;
