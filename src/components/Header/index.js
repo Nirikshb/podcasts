069d0cde004d8d9e98aaaef8e5b64860b5ca3153
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-08-20 20:48:29 +0530

    Initial commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 const Header = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  console.log("Current Path");
+
+
   return (
     <div className="navbar">
-        <div className="links">
-            
-        </div>
+       <div className='gradient'>
+         <div className="links">
+          <Link to="/" className={currentPath =="/" ? "active" : "" }>SignUp</Link>   
+  
+          <Link to="/start-a-podcast"
+          className={currentPath == "/podcasts" ? "active" : ""}>Start A Podcast</Link>   
+          
+          <Link to="/profile"
+          className={currentPath == "/start-a-podcast" ? "active" : ""}>Profile</Link>   
+         </div>
+       </div>
     </div>
   )
 }
