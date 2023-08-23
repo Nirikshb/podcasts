e327d2af491bc1d34da241bf5eff546032438ff9
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-08-23 21:21:00 +0530

    Initial commit

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -3,11 +3,14 @@ import InputComponent from '../components/Input'
 
 const Signup = () => {
     const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-  return (
+    return (
     <div>
         <Header />
-     <div>
+        <h1>Signup</h1>
         <InputComponent 
          state={fullName}
          setState={setFullName}
@@ -15,7 +18,31 @@ const Signup = () => {
          type="text"
          required={true}
         />
-     </div>
+
+        <InputComponent
+        state={email}
+        setState={setEmail}
+        placeholder="Full Name"
+        type="text"
+        required={true}
+        />
+
+        <InputComponent
+        state={password}
+        setState={setPassword}
+        placeholder="Password"
+        type="password"
+        required={true}
+        />
+
+      
+        <InputComponent
+        state={confirmPassword}
+        setState={setConfirmPassword}
+        placeholder="Confirm Password"
+        type="password"
+        required={true}
+        />  
     </div>
   )
 }
