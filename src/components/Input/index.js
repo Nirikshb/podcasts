3d3ccf1a37d80ef95dc53536fe06200700667773
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-09-27 08:01:18 +0530

    Initial commit

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const InputComponent = ({ type, state, setState,
-placeholder,required}) => {
+const InputComponent = ({ type, state, setState, placeholder, required }) => {
   return (
     <input
-    type={type}
-    value={state}
-    onChange={(e)=>setState(e.target.value)}
-    placeholder={placeholder}
-    required={required}
-    className="custom-input"
+      type={type}
+      value={state}
+      onChange={(e) => setState(e.target.value)}
+      placeholder={placeholder}
+      required={required}
+      className="custom-input"
     />
-  )
-}
+  );
+};
 
 export default InputComponent;
