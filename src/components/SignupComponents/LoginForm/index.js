83cfa06a6d341c6cd2aea12b18973e866640b381
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-09-30 09:19:16 +0530

    Initial commit

--- src/components/SignupComponents/LoginForm/index.js
+++ src/components/SignupComponents/LoginForm/index.js
@@ -1,53 +1,63 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Button from "../../Common/Button";
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import {auth} from "../../../firebase";
-import { getDoc } from 'firebase/firestore';
-
+import { signInWithEmailAndPassword } from 'firebase/auth'; // Import Firebase authentication function
+import { auth } from "../../../firebase"; // Import Firebase authentication object
+import { getDoc, doc } from 'firebase/firestore'; // Import Firestore functions
 
 const LoginForm = () => {
+    // Define state variables to store email and password
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin = async ()=> {
-        console.log("handling login");
+    // Function to handle the login process
+    const handleLogin = async () => {
+        console.log("Handling login");
 
-        try{
+        try {
+          // Attempt to sign in the user with the provided email and password
           const userCredential = await signInWithEmailAndPassword(
             auth,
             email,
             password
           );
 
+          // Retrieve the authenticated user
           const user = userCredential.user;
+
+          // Retrieve user data from Firestore
           const userDoc = await getDoc(doc(db, "user", user.uid));
           const userData = userDoc.data();
-          console.log("userData", userData);
+          console.log("User data:", userData);
 
+          // Dispatch an action to set the user in the Redux store (if applicable)
           dispatch(
             setUser({
-              name : userData.name,
-              email:user.email,
+              name: userData.name,
+              email: user.email,
               uid: user.uid,
             })
           );
 
+          // Display a success message (you may use a toast library for this)
           toast.success("User Login Successful");
-          setLoading(false);
-          navigate("/profile");//navigating to the profile page
-        } catch (error){
+
+          // Perform navigation to the profile page (you may use a routing library)
+          setLoading(false); // Assuming 'setLoading' is a function that manages loading state
+          navigate("/profile"); // Navigating to the profile page
+        } catch (error) {
+          // Handle errors during login
           console.error("Error signing in", error);
-          setLoading(false);
-          toast.error(error.message);
+          setLoading(false); // Assuming 'setLoading' is a function that manages loading state
+          toast.error(error.message); // Display an error message (you may use a toast library)
         }
     };
 
-     return (
-    <div>
-      {/* todo : incorprate ui for login maybe use toastify for login pop ups */}
-    </div>
-  )
+    return (
+      <div>
+        {/* TODO: Incorporate UI for login, such as input fields for email and password */}
+        {/* TODO: You can use a toast library for displaying login pop-ups */}
+      </div>
+    );
 }
 
-export default index
+export default index;
