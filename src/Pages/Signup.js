ee25c8b5de59af57be9425f30f0cb8dea689ebfc
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-01 08:28:14 +0530

    Initial commit

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import InputComponent from '../components/Input'
-import SignupForm from '../components/SignupComponents/SignupForm';
+import React, { useState } from 'react';
+import InputComponent from '../components/Input'; // Import InputComponent
+import SignupForm from '../components/SignupComponents/SignupForm'; // Import SignupForm component
 
 const Signup = () => {
-    
+    //variable 'flag' to toggle between signup and login views
     const [flag, setFlag] = useState(false);
 
+    // Function to handle signup
     const handleSignup = () =>{
       console.log("Signing up handled");
     };
@@ -13,20 +14,27 @@ const Signup = () => {
     return (
     <div>
         <Header />
-      <div className='input-wrapper'>  
-        {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
-        {!flag ? <SignupForm /> : <LoginForm />}
         
-        {!flag ? (
-          <p style={{cursor:"pointer"}} onClick={() => setFlag(!flag)}>
-            Already have an Account? Click here to Login.
-          </p>
-        ) : (
-          <p onClick={() => setFlag(!flag)}>
-            Don't have an Account? Click here to signup.
-          </p>
-        )}
-      </div>
+        {/* Container for signup/login forms and toggling */}
+        <div className='input-wrapper'>  
+            
+            {/* Display 'Signup' or 'Login' heading based on 'flag' */}
+            {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
+            
+            {/* Display 'SignupForm' or 'LoginForm' based on 'flag' */}
+            {!flag ? <SignupForm /> : <LoginForm />}
+            
+            {/* Display a message and toggle link based on 'flag' */}
+            {!flag ? (
+              <p style={{cursor:"pointer"}} onClick={() => setFlag(!flag)}>
+                Already have an Account? Click here to Login.
+              </p>
+            ) : (
+              <p onClick={() => setFlag(!flag)}>
+                Don't have an Account? Click here to signup.
+              </p>
+            )}
+        </div>
     </div>
   )
 }
