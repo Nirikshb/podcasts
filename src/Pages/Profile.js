35cf1f62cbe26bed1969e74b9b3b11400636ea16
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-05 11:05:50 +0530

    Initial commit

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -7,6 +7,20 @@ const Profile = () => {
   const user = useSelector((state) => state.user.user);
   console.log("My user", user);
 
+  if(!user){
+    return <p>Loading....</p>;
+  }
+
+  const handleLogout = ()=>{
+    signOut(auth).then(()=>{
+      //signed out 
+      toast.success("User Logged Out")
+    }).catch((error)=>{
+      //the error
+      toast.error(error)
+    });
+
+  };
 
   return (
     <div>
@@ -14,6 +28,8 @@ const Profile = () => {
       <h1>{user.name}</h1>
       <h1>{user.email}</h1>
       <h1>{user.uid}</h1>
+
+      <Button text={Logout} onClick={handleLogout} />
     </div>
   );
 }
