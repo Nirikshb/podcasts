1d98d37c30b210741ce1bf979db9509d1d7a2962
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-09 19:54:20 +0530

    Initial commit

--- src/components/StartAPodcast/CreatePodcastForm.js
+++ src/components/StartAPodcast/CreatePodcastForm.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const CreatePodcastForm = () => {
-    const [title, setTitle] = useState('');
-    const [desc, setDesc] = useState('');
-    const [displayImage, setDisplayImage] = useState("");
-    const [loading, setLoading] = useState(false);
+    // State variables to manage form inputs and other stateful data
+    const [title, setTitle] = useState(''); // State for podcast title
+    const [desc, setDesc] = useState(''); // State for podcast description
+    const [displayImage, setDisplayImage] = useState(''); // State for podcast image
+    const [loading, setLoading] = useState(false); // State for loading indicator
+
+    // React Router's useNavigate hook for navigation
     const navigate = useNavigate();
+
+    // Redux's useDispatch hook for dispatching actions
     const dispatch = useDispatch();
-  return (
-    <div>CreatePodcastForm</div>
-  )
-}
+
+    return (
+        <div>
+            <div>CreatePodcastForm</div>
+        </div>
+    );
+};
 
 export default CreatePodcastForm;
