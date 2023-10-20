bb1b54a6c2740e101d94a731a35193446290c2f5
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-20 23:36:27 +0530

    Create-An-Episode

--- src/Pages/CreateAnEpisodePage.js
+++ src/Pages/CreateAnEpisodePage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Common/Header";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 
 function CreateAnEpisodePage() {
+    
+    const [title, setTitle]= useState("");
+    const [desc, setDesc] = useState("");
+    const [displayImage, setDisplayImage]= useState();
+    const [bannerImage, setBannerImage]= useState();
+
+    const [loading, setLoading] =useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    
+    
+    
     return (
         <div>
             <Header />
