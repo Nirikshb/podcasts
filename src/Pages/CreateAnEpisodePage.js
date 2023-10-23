00831c513f87e1ac2d3e872a4fac983885b56eb7
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-23 20:50:11 +0530

    Initial commit

--- src/Pages/CreateAnEpisodePage.js
+++ src/Pages/CreateAnEpisodePage.js
@@ -1,27 +1,34 @@
+// Import necessary dependencies and components
 import React, { useState } from "react";
 import Header from "../components/Common/Header";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-
+// Create a functional component named CreateAnEpisodePage
 function CreateAnEpisodePage() {
-    
-    const [title, setTitle]= useState("");
-    const [desc, setDesc] = useState("");
-    const [displayImage, setDisplayImage]= useState();
-    const [bannerImage, setBannerImage]= useState();
+    // Initialize state variables using the useState hook
+    const [title, setTitle] = useState("");        // State for episode title
+    const [desc, setDesc] = useState("");          // State for episode description
+    const [displayImage, setDisplayImage] = useState();  // State for display image
+    const [bannerImage, setBannerImage] = useState();    // State for banner image
 
-    const [loading, setLoading] =useState(false);
+    const [loading, setLoading] = useState(false);   // State to manage loading state
+
+    // Get the navigate function from react-router-dom to handle navigation
     const navigate = useNavigate();
+
+    // Get the dispatch function from Redux to dispatch actions
     const dispatch = useDispatch();
-    
-    
-    
+
+    // Render the component
     return (
         <div>
+            {/* Render the Header component */}
             <Header />
+            {/* Add your form or other components here to create an episode */}
         </div>
     );
 }
 
+// Export the CreateAnEpisodePage component
 export default CreateAnEpisodePage;
