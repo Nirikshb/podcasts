ee0910a52e58bdd2dd2bc16ae5b289c93c92e046
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-10-24 21:51:10 +0530

    Initial commit

--- src/components/Common/Button/index.js
+++ src/components/Common/Button/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 const Button = ({ text, onClick, disabled }) => {
   return (
-    <div onClick={onClick} className={`custom-btn${disabled ? ' disabled' : ''}`}>
-      {text}
-    </div>
+    /* The <button> element with an onClick event handler, custom CSS classes, and disabled attribute */
+    <button onClick={onClick} className={`custom-btn${disabled ? ' disabled' : ''}`} disabled={disabled}>
+      {text} {/* Display the button text */}
+    </button>
   );
 };
 
