54f35f1c3d2b1c86f2528068d791db969b0a62c4
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-11-02 23:06:36 +0530

    Initial commit

--- src/components/Common/PrivareRoutes.js
+++ src/components/Common/PrivareRoutes.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { useAuthState } from 'your-auth-library'; // Import your auth library
+import { useAuthState } from 'your-auth-library'; // Import your authentication library
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoutes = () => {
+    // Use the useAuthState hook to check the user's authentication state
     const [user, loading, error] = useAuthState(auth);
 
+    // Check the different conditions for rendering content
     switch (true) {
         case loading:
+            // Display a loading message while authentication is in progress
             return <p>Loading....</p>;
         case !user || error:
+            // If there is no user authenticated or an error occurred, redirect to the home page
             return <Navigate to="/" replace />;
         default:
+            // If the user is authenticated and there are no errors, render the child routes (Outlet)
             return <Outlet />;
     }
 };
