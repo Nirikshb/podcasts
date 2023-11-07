820818b220d1bcb81fb2e1b54ead493d983bb6d3
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-11-07 23:13:30 +0530

    Initial commit

---
+++ src/Pages/Podcasts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Podcasts = () => {
+  return (
+    <div>
+        <Header />
+        <h1>Discover Podcast</h1>
+        </div>
+  )
+}
+
+export default Podcasts;
