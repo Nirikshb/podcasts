6457ec6a9e7918e6e39bac154b39f4a4a79c4127
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-11-15 23:12:38 +0530

    Initial commit

--- src/slices/PodcastSlice.js
+++ src/slices/PodcastSlice.js
@@ -1,18 +1,18 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    podcasts:[],
+    podcasts: [], // Initial state with an empty array for podcasts
 };
 
-const podcastSlice = createSlice ({
-    name : "podcasts",
-    initialState,
-    reducers :{
-        setPodcasts : (state,action) =>{
-            state.podcasts = action.payload;
+const podcastSlice = createSlice({
+    name: "podcasts", // Slice name for Redux DevTools
+    initialState, // Initial state for the slice
+    reducers: {
+        setPodcasts: (state, action) => {
+            state.podcasts = action.payload; // Update podcasts with payload data
         },
     },
 });
 
-export const {setPodcasts} = podcastSlice.actions;
-export default podcastSlice.render;
+export const { setPodcasts } = podcastSlice.actions; // Action creator
+export default podcastSlice.reducer; // Reducer function
