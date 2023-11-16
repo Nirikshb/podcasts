42f55eceff86301e1bac751fe5c2dd12570810bc
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-11-16 23:36:20 +0530

    Initial commit

--- src/components/SignupComponents/SignupForm/index.js
+++ src/components/SignupComponents/SignupForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { toast } from 'react-toastify'; // Import toast for displaying error messages
+import { toast } from 'react-toastify';
 import InputComponent from '../../Common/Input';
 import Button from '../../Common/Button';
 import { auth, db } from '../../../firebase';
@@ -22,28 +22,29 @@ const SignupForm = () => {
   const handleSignup = async () => {
     console.log('Handling Signup...');
 
-    if (password === confirmPassword && 
-        password.length >= 6 &&
-        fullName &&
-        email) 
-        {
+    if (
+      password === confirmPassword &&
+      password.length >= 6 &&
+      fullName &&
+      email
+    ) {
       try {
-        // Creating user's account
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
         const user = userCredential.user;
-        console.log('user', user);
 
-        // Define fileUrl here or wherever you get it
-        const fileUrl = 'your_profile_pic_url';
+        const fileUrl = 'your_profile_pic_url'; // Define fileUrl
 
         await setDoc(doc(db, 'users', user.uid), {
-          name: fullName, // Fixed the variable name here
+          name: fullName,
           email: user.email,
           uid: user.uid,
-          profilePic: fileUrl, // Fixed the variable name here
+          profilePic: fileUrl, // Assign fileUrl
         });
 
-        // Saving data in redux, calls the redux action
         dispatch(
           setUser({
             name: fullName,
@@ -52,66 +53,64 @@ const SignupForm = () => {
           })
         );
 
-        toast.success("Yippie kay-yay Login Created")
+        toast.success('Account created successfully');
         navigate('/profile');
         console.log('user', user);
-      } catch (e) {
-        console.log('error', e);
-        toast.error(e.message);
+      } catch (error) {
+        console.log('error', error);
+        toast.error(error.message);
         setLoading(false);
       }
     } else {
       if (password !== confirmPassword) {
-        toast.error('Please Make Sure Your Passwords Match');
-      }
-      else if (password.length < 6) {
-        toast.error('Make sure password length is at least 6 characters');
+        toast.error('Passwords do not match');
+      } else if (password.length < 6) {
+        toast.error('Password must be at least 6 characters long');
       }
     }
   };
-        
 
   return (
-    <> 
-    <InputComponent 
-    state={fullName}
-    setState={setFullName}
-    placeholder="Full Name"
-    type="text"
-    required={true}
-   />
+    <>
+      <InputComponent
+        state={fullName}
+        setState={setFullName}
+        placeholder="Full Name"
+        type="text"
+        required={true}
+      />
 
-   <InputComponent
-   state={email}
-   setState={setEmail}
-   placeholder="Full Name"
-   type="text"
-   required={true}
-   />
+      <InputComponent
+        state={email}
+        setState={setEmail}
+        placeholder="Email"
+        type="email"
+        required={true}
+      />
 
-   <InputComponent
-   state={password}
-   setState={setPassword}
-   placeholder="Password"
-   type="password"
-   required={true}
-   />
+      <InputComponent
+        state={password}
+        setState={setPassword}
+        placeholder="Password"
+        type="password"
+        required={true}
+      />
 
- 
-   <InputComponent
-   state={confirmPassword}
-   setState={setConfirmPassword}
-   placeholder="Confirm Password"
-   type="password"
-   required={true}
-   />  
-   
-   <Button text={loading ? "Loading..." : "Signup"} 
-   disabled={loading} 
-   onclick={handleSignup} />
-   
-   </>
-  )
-}
+      <InputComponent
+        state={confirmPassword}
+        setState={setConfirmPassword}
+        placeholder="Confirm Password"
+        type="password"
+        required={true}
+      />
+
+      <Button
+        text={loading ? 'Loading...' : 'Signup'}
+        disabled={loading}
+        onClick={handleSignup} // Corrected 'onclick' to 'onClick'
+      />
+    </>
+  );
+};
 
 export default SignupForm;
